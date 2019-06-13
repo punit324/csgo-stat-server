3f2f7300246bd01ecd70f7aa1849ccccfacae115
punit <punitpatel324@gmail.com>
2019-06-13 23:02:11 +0530

    added log message on request

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ app.listen(3000, () => {console.log("Server started at 3000...")});
 app.use(Cors());
 
 app.get('/getSteamData', (request, response,  next) => {
+	console.log('request received with steamId : ' + request.query.steamId);
 	var key = '257AEBD49E4964B1F1B79EA29E992B2F';
 	Https.get('https://api.steampowered.com/ISteamUserStats/GetUserStatsForGame/v0002/?appid=730&key=' + key + '&steamid=' + request.query.steamId + '&format=json' , (res) => {
 	res.once("data", function(data){
